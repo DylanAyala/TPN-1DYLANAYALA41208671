54226f0fd96a63155f52e8cfdf0223c4560f9358
DylanAyala <44531944+DylanAyala@users.noreply.github.com>
2019-04-02 18:45:27 -0300

    Add files via upload

--- operations.h
+++ operations.h
@@ -1,9 +1,49 @@
 #ifndef OPERATIONS_H_INCLUDED
 #define OPERATIONS_H_INCLUDED
-
+/**
+ *
+ * \param int num1
+ * \param int num2
+ * \return int con la suma de num1 y num2
+ *
+ */
 int operatationSum(int num1, int num2);
+
+
+/** \brief
+ *
+ * \param int num1
+ * \param int num2
+ * \return int con la resta de num1 y num2
+ *
+ */
 int operatationResta(int num1, int num2);
+/** \brief
+ *
+ * \param int num1
+ * \param int num2
+ * \return int con la multiplicacion de num1 y num2
+ *
+ */
 int operatationMultiply(int num1, int num2);
+
+/** \brief
+ *
+ * \param int num1
+ * \param int num2
+ * \return float con la divicion de num1 y num2
+ *
+ */
+
 float operatationDivide(int num1, int num2);
+
+/** \brief
+ *
+ * \param int num1
+ * \param int num2
+ * \return float con la divicion de num1 y num2
+ *
+ */
+
 long operatationFactorial(int num);
 #endif // OPERATIONS_H_INCLUDED
